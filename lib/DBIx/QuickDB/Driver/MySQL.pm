8a650d300d841f6668aa441c78d28149989f62ea
Chad Granum <exodist7@gmail.com>
2026-07-10 11:09:22 -0700

    Automated Version Bump

--- lib/DBIx/QuickDB/Driver/MySQL.pm
+++ lib/DBIx/QuickDB/Driver/MySQL.pm
@@ -2,7 +2,7 @@ package DBIx::QuickDB::Driver::MySQL;
 use strict;
 use warnings;
 
-our $VERSION = '0.000053';
+our $VERSION = '0.000054';
 
 use Capture::Tiny qw/capture/;
 use Carp qw/confess croak/;
